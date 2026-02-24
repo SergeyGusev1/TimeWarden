31e9d7e82c6984a96d3d00bd238feb325459c553
Сергей Гусев <magnatuch@vk.com>
2026-02-24 19:31:12 +0300

    Настройка alembic, gitignore, убрал лишние файлы которые не должны побадать в git.

--- app/models/activity.py
+++ app/models/activity.py
@@ -1,14 +1,16 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
+from datetime import datetime
 from app.core.database import Base
 
 
-class activity(Base):
-    __tablename__='activities'
+class Activity(Base):
+    __tablename__ = 'activities'
 
-    id = Column(Integer, primary_key=True)
-    app_name = Column(String, index=True)
-    window_title = Column(String)
-    category = Column(String, nullable=True)
-    duretion_seconds = Column(Float)
-    start_time = Column(DateTime)
-    end_time = Column(DateTime)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    app_name: Mapped[str] = mapped_column(index=True)
+    window_title: Mapped[str]
+    category: Mapped[Optional[str]]
+    duretion_seconds: Mapped[float]
+    start_time: Mapped[datetime]
+    end_time: Mapped[datetime]
