d6ce79a3bb6c8408a87f1e3140a4360d1ae74199
Сергей Гусев <magnatuch@vk.com>
2026-03-03 15:44:46 +0300

    Эндпоинты, сервисы, круд для статистики

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
-from app.api.v1.active_endpoints import router as activity_router
+from app.api.v1 import router
 
 
 app = FastAPI(title=settings.APP_NAME)
-app.include_router(activity_router, prefix='/api/v1', tags=['Activities'])
+app.include_router(router)
 
 
 @app.get('/')
